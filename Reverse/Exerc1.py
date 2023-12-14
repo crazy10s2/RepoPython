bae1a3fb6e9bec73c466e3efe2d196c619ccecab
crazy10s2 <150177425+crazy10s2@users.noreply.github.com>
2023-12-14 17:00:45 -0300

    Update Exerc1.py

--- Reverse/Exerc1.py
+++ Reverse/Exerc1.py
@@ -4,7 +4,3 @@ class InversorDeString:
 
     def inverter(self):
         return self.texto[::-1]
-
-texto = input("Digite uma string: ")
-inversor = InversorDeString(texto)
-print(inversor.inverter())
