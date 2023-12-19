975f997a1131ceaa335f0cbd44a3775dd03fc5f8
crazy10s2 <cauadm1319@gmail.com>
2023-12-19 12:31:48 -0300

    Commit final

--- teste/teste1.py
+++
@@ -1,8 +0,0 @@
-class RegraDeTres:
-    def __init__(self, a, b, c):
-        self.a = a
-        self.b = b
-        self.c = c
-
-    def calcular(self):
-        return (self.b * self.c) / self.a
